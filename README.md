cf43c3c375413f13379789d1c08fd67f3ffc19a8
Patrick Allen <github.com@eth0.uk.net>
2015-08-29 20:17:31 +0100

    Protocol-neutral external links

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # Hideously pre-release :) 
 
 TODO:
+Advise on getting Id/UserId
 My Monitors > Edit Monitor > Uptime Button Code
 	Id=15788&UserId=195073
 
--- siteuptime.php
+++ siteuptime.php
@@ -68,7 +68,7 @@ $result = mysqli_query($con,"SELECT * FROM `siteuptime` WHERE `timestamp` > DATE
 <head>
     <title>SiteUptime Endpoint</title>
     <link rel="stylesheet" type="text/css" href="siteuptime.css">
-    <script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['corechart']}]}"></script>
+    <script type="text/javascript" src="//www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['corechart']}]}"></script>
     <script type="text/javascript">
     window.onresize = function(){
         // location.reload();
@@ -81,7 +81,7 @@ $result = mysqli_query($con,"SELECT * FROM `siteuptime` WHERE `timestamp` > DATE
 
 <header>
     <h1><span id="Site">Site</span><span id="Uptime">Uptime</span> Endpoint</h1>
-    <a href="https://github.com/ethzero"><img style="position: fixed; top: 0; right: 0; border: 0; z-index: 1" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+    <a href="https://github.com/ethzero"><img style="position: fixed; top: 0; right: 0; border: 0; z-index: 1" src="//camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="//s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
 </header>
 
 <section>
@@ -123,11 +123,11 @@ $result = mysqli_query($con,"SELECT * FROM `siteuptime` WHERE `timestamp` > DATE
 
 <footer>
     <div class="panel" id="siteuptime">
-        <a HREF="http://www.SiteUptime.com/"><img SRC="http://www.siteuptime.com/images/Siteuptime-Button-Blue.gif" BORDER="0" WIDTH="88" HEIGHT="31" ALT="SiteUptime Web Site Monitoring Service"></a>
-        <a href='http://www.siteuptime.com/' target="_blank" onMouseOver='this.href="http://www.siteuptime.com/statistics.php?Id=<?php echo $cfg['siteuptime']['Id']?>&UserId=<?php echo $cfg['siteuptime']['UserId']?>";'><img width=85 height=16 border=0 alt='website uptime' src="http://btn.siteuptime.com/genbutton.php?u=<?php echo $cfg['siteuptime']['UserId']?>&m=<?php echo $cfg['siteuptime']['Id']?>&c=blue&p=total"></a><noscript><a href='http://www.siteuptime.com/'>website monitoring</a></noscript>
+        <a HREF="//www.SiteUptime.com/"><img SRC="//www.siteuptime.com/images/Siteuptime-Button-Blue.gif" BORDER="0" WIDTH="88" HEIGHT="31" ALT="SiteUptime Web Site Monitoring Service"></a>
+        <a href='http://www.siteuptime.com/' target="_blank" onMouseOver='this.href="//www.siteuptime.com/statistics.php?Id=<?php echo $cfg['siteuptime']['Id']?>&UserId=<?php echo $cfg['siteuptime']['UserId']?>";'><img width=85 height=16 border=0 alt='website uptime' src="//btn.siteuptime.com/genbutton.php?u=<?php echo $cfg['siteuptime']['UserId']?>&m=<?php echo $cfg['siteuptime']['Id']?>&c=blue&p=total"></a><noscript><a href='//www.siteuptime.com/'>website monitoring</a></noscript>
     </div>
     <div class="panel" id="cc">
-        <a rel="license" style="display: block" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Licence" style="border-width:0; margin-right: 0.5em;" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">SiteUptime Endpoint</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://eth0.uk.net" property="cc:attributionName" rel="cc:attributionURL">Patrick "ethzero" Allen</a> is licensed under a <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>
+        <a rel="license" style="display: block" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Licence" style="border-width:0; margin-right: 0.5em;" src="//i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">SiteUptime Endpoint</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://eth0.uk.net" property="cc:attributionName" rel="cc:attributionURL">Patrick "ethzero" Allen</a> is licensed under a <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>
     </div>
     <div class="panel" id="github">
         <a class="github-button" href="https://github.com/ethzero" data-style="mega" aria-label="Follow @ethzero on GitHub">Follow @ethzero</a>
@@ -136,7 +136,7 @@ $result = mysqli_query($con,"SELECT * FROM `siteuptime` WHERE `timestamp` > DATE
     <p id="legal">The SiteUptime Endpoint script is not affiliated with or endorsed by SiteUptime. "SiteUptime" is a registered trademark of SiteUptime LLC.</p>
 </footer>
 
-<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
+<script async defer id="github-bjs" src="//buttons.github.io/buttons.js"></script>
 <script type="text/javascript">
 google.setOnLoadCallback(drawChart);
 function drawChart() {
